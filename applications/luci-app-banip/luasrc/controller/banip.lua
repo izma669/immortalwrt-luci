193fabf43f22525f28f62152cbd77809813fd7b8
ZXH <42003399+izma669@users.noreply.github.com>
2026-08-29 01:20:04 +0800

    Update banip.lua

--- applications/luci-app-banip/luasrc/controller/banip.lua
+++ applications/luci-app-banip/luasrc/controller/banip.lua
@@ -3,11 +3,11 @@
 module("luci.controller.banip", package.seeall)
 
 function index()
-	entry({"admin", "services", "banip"}, firstchild(), _("banIP"), 60).acl_depends = { "luci-app-banip" }
-	entry({"admin", "services", "banip", "overview"}, view("banip/overview"), _("Overview"), 10)
-	entry({"admin", "services", "banip", "ipsetreport"}, view("banip/ipsetreport"), _("IPSet Report"), 20)
-	entry({"admin", "services", "banip", "blacklist"}, view("banip/blacklist"), _("Edit Blacklist"), 30)
-	entry({"admin", "services", "banip", "whitelist"}, view("banip/whitelist"), _("Edit Whitelist"), 40)
-	entry({"admin", "services", "banip", "maclist"}, view("banip/maclist"), _("Edit Maclist"), 50)
-	entry({"admin", "services", "banip", "logread"}, view("banip/logread"), _("Log View"), 60)
+	entry({"admin", "control", "banip"}, firstchild(), _("banIP"), 60).acl_depends = { "luci-app-banip" }
+	entry({"admin", "control", "banip", "overview"}, view("banip/overview"), _("Overview"), 10)
+	entry({"admin", "control", "banip", "ipsetreport"}, view("banip/ipsetreport"), _("IPSet Report"), 20)
+	entry({"admin", "control", "banip", "blacklist"}, view("banip/blacklist"), _("Edit Blacklist"), 30)
+	entry({"admin", "control", "banip", "whitelist"}, view("banip/whitelist"), _("Edit Whitelist"), 40)
+	entry({"admin", "control", "banip", "maclist"}, view("banip/maclist"), _("Edit Maclist"), 50)
+	entry({"admin", "control", "banip", "logread"}, view("banip/logread"), _("Log View"), 60)
 end
