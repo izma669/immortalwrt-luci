39b10290a359a769e001d7ad8123cff2315cdc77
ZXH <42003399+izma669@users.noreply.github.com>
2026-09-04 15:37:24 +0800

    Update vhusbd.lua

--- applications/luci-app-virtualhere/luasrc/model/cbi/vhusbd.lua
+++ applications/luci-app-virtualhere/luasrc/model/cbi/vhusbd.lua
@@ -5,17 +5,12 @@
 local fs = require "nixio.fs"
 
 m = Map("vhusbd", translate("VirtualHere USB Server"), translate(
-            "<br /> VirtuslHere USB Server用途：<br />可实现通过网络挂载USB设备。<br /><br />使用方法：<br />1.将需要挂载的USB设备插在路由器的USB口并启用服务器。<br />2.客户端主机从<a href='http://www.virtualhere.com/usb_client_software' target='_blank'>官网</a>下载对应的客户端程序运行即可。<br />3.离线注册软件以获得更多的链接授权<br /><br /> 注册格式:<br /><span class='blink-red-yellow'>xxxxxxxxxxxx</span>,<span class='blink-blue-yellow'>999</span>,<span class='blink-green-yellow'>MCACDkn0jww6R5WOIjFqU/apAg4Um+mDkU2TBcC7fA1FrA==</span><br /><br />红色字部分是你的服务器序列号,蓝色是你想解锁授权的数量,绿色是key<br /><br />注册方式：<br /><br />启动客户端连接到服务器后将获取到的12位序列号替换上面的注册代码中红色部分完成注册即可  <br /><br />" ..
+            "<br /> VirtuslHere USB Server用途：<br />可实现通过网络挂载USB设备。<br /><br />注册方法：<br />1.从<a href='http://www.virtualhere.com/usb_client_software' target='_blank'>官网</a>下载对应平台的客户端程序运行并联机,从对应服务器获取序列号。<br />2.将下面的闪烁部分替换为之前获取到的序列号以获得完整的注册代码<br />3.最后将完整的注册代码输入到客户端的注册入口提交即可:<br /><br />注册代码格式:<br /><span class='blink-red-yellow'>服务器序列号</span>,<span style='color:blue;'>999</span>,<span style='color:blue;'>MCACDkn0jww6R5WOIjFqU/apAg4Um+mDkU2TBcC7fA1FrA==</span><br /><br />" ..
             "<style> " ..
             ".blink-red-yellow { animation: blink-red-yellow 1s step-end infinite; } " ..
             "@keyframes blink-red-yellow { 0%, 100% { color: red; } 50% { color: yellow; } } " ..
-            ".blink-blue-yellow { animation: blink-blue-yellow 1s step-end infinite; } " ..
-            "@keyframes blink-blue-yellow { 0%, 100% { color: blue; } 50% { color: yellow; } } " ..
-            ".blink-green-yellow { animation: blink-green-yellow 1s step-end infinite; } " ..
-            "@keyframes blink-green-yellow { 0%, 100% { color: green; } 50% { color: yellow; } } " ..
             "</style>"
 ))
-
 -- Basic config
 m:section(SimpleSection).template = "vhusbd/status"
 
@@ -32,6 +27,13 @@ Access.rmempty = false
 -- 保存配置后自动重载服务
 function m.on_after_commit(self)
     luci.sys.call("/etc/init.d/vhusbd reload >/dev/null 2>&1")
+end
+
+-- ===== 在 Settings 区域末尾添加手动重启按钮 =====
+local restart_btn = s:option(Button, "restart_btn", translate("重载服务"))
+restart_btn.inputstyle = "reload"
+function restart_btn.write(self, section)
+    luci.sys.call("/etc/init.d/vhusbd restart >/dev/null 2>&1")
 end
 
 return m
